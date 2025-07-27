4da84b63208fe12fffe527863bb53f0ef0cc47f6
Smb2003 <99506103+Smb2003@users.noreply.github.com>
2025-07-27 20:58:12 +0500

    Added deploy link of backend

--- src/config/AxiosInstance.jsx
+++ src/config/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api/v1/users",
+    baseURL: "https://aivirtual-assistant-backend.vercel.app/api/v1/users",
     withCredentials: true
 });
 
