e70797299c83387170db894647955e6e4d2abd69
Smb2003 <99506103+Smb2003@users.noreply.github.com>
2025-07-27 21:41:50 +0500

    Update Ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -141,7 +141,7 @@ const HomePage = () => {
           <img src={AudioGif} alt="" className='w-[150px] h-[100px]' />
         </div>
         <div>
-          {/* <div><button className='text-white' onClick={()=>{speakText(geminiResponse?.response)}}>Speak</button></div> */}
+          {listening && <div><p>listening</p></div>}
           {(transcript)&&
             <p className="text-white font-xl ">{geminiResponse?.response}</p>}
         </div>
